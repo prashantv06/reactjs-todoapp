2d82284a1a82d33f37ad7841c2562aabc7cc9014
Prashant <prasant.irl@gmail.com>
2020-08-18 11:53:28 +0100

    Defining Style and State

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -15,22 +15,40 @@ import PropTypes from 'prop-types';
 
 export class Todoitem extends Component {
 
-    getStyle = () => {
+    /*getStyle = () => {
         if(this.props.todo.completed){
             return{
-                textDecoration: 'line-through'
+                textDecoration: 'line-through',
+                color: 'red',
+                backgroundColor: '#f4f4f4',
+                fontSize: '20px'
             }
         }
         else{
             return{
-                textDecoration: 'none'
+                textDecoration: 'none',
+                color: 'blue',
+                backgroundColor: '#f4f4f4',
+                fontSize: '20px'
             }
+        }
+    }*/
+
+
+    getStyle = () => {
+        return{
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+            color: this.props.todo.completed ? 'red' : 'blue'
         }
     }
 
     render() {
         return (
             <div style={this.getStyle()}>
+                <input type="checkbox" name="" id="" />
                 <p>{ this.props.todo.title }</p>
             </div>
         )
